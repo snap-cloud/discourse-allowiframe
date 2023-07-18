d8f2f790dde80a9110fe6ce9115643e8eec7a91a
Michael Ball <michael@mball.co>
2023-07-19 00:00:50 +0200

    Move everything to after_initialize

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,18 @@
-# name: Allow iFrame embedding
+# name: allowiframe
 # about: Changes X-Frame-Options so the site can be embedded
-# version: 1
+# version: 1.1
 # authors: bex-team, riking,
 
-Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
+after_initialize do
+  # Directly modify the discourse rails defaults
+  # Change the default headers to disable browsers' flawed legacy XSS protection.
+  Rails.application.config.action_dispatch.default_headers = {
+    # "X-Frame-Options" => "SAMEORIGIN",
+    "X-XSS-Protection" => "0",
+    "X-Content-Type-Options" => "nosniff",
+    "X-Download-Options" => "noopen",
+    "X-Permitted-Cross-Domain-Policies" => "none",
+    "Referrer-Policy" => "strict-origin-when-cross-origin",
+  }
+
+end
