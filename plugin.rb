64c450740014914066454943a44f6069d194d9a5
Michael Ball <cycomachead@gmail.com>
2024-01-30 23:02:54 -0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,12 +7,12 @@ after_initialize do
   # Directly modify the discourse rails defaults
   # Change the default headers to disable browsers' flawed legacy XSS protection.
   Rails.application.config.action_dispatch.default_headers = {
-    # "X-Frame-Options" => "SAMEORIGIN",
+    "X-Frame-Options" => "ALLOW snapcon.org",
     "X-XSS-Protection" => "0",
     "X-Content-Type-Options" => "nosniff",
     "X-Download-Options" => "noopen",
     "X-Permitted-Cross-Domain-Policies" => "none",
     "Referrer-Policy" => "strict-origin-when-cross-origin",
+    "x-michael-test" => "true",
   }
-
 end
